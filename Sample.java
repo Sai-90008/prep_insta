263932656a6d25c6ddf5ca05e6c904738a493700
Sai90008 <saisai90008@gmail.com>
2026-02-04 21:14:29 +0530

    first githttps://github.com/Sai-90008/prep_insta.git

---
+++ Sample.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Sample{
+    public static void main(String [] args){
+        System.out.print("Enter the number: ");
+        Scanner s=new Scanner(System.in);
+        int num=s.nextInt();
+        int rev=0;
+       while(num!=0){
+        
+        rev=(rev*10)+(num%10);
+        num=num/10;
+
+       }
+       System.out.println(rev);
+
+
+
+    }
+}
